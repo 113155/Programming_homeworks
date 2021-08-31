771332ea54f4931393ecd2f9b98cec1083fc7a2f
113155 <89810522+113155@users.noreply.github.com>
2021-08-31 13:22:10 -0500

    Initial commit

---
+++ homework1.js
@@ -0,0 +1,9 @@
+let saleman = prompt('Enter salaried or not salaried', '');
+// if (saleman === 'salaried'){
+//     if (sales < 300){
+//         sales *= 0;
+//         return salesCommission;
+//     } else if (sales > 300 && sales < 500){
+//         sales *= 1/100
+//     }
+// }
