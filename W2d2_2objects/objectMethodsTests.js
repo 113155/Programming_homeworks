024dc174ef8cefd98b70822d356b970bf1b07dc6
Deborah Igaba <89810522+113155@users.noreply.github.com>
2021-09-08 02:03:11 -0500

    Create objectMethodsTests.js

---
+++ W2d2_2objects/objectMethodsTests.js
@@ -0,0 +1,50 @@
+
+const assert = require("assert");  //always need this with node
+const myExports = require("./objectMethods.js");  //with node need the name of your file with your functions here
+const calculator = myExports.calculator;  //do this for all of the functions used in the Mocha tests
+const Accumulator = myExports.Accumulator; 
+
+
+describe("test calculator with values 2, 3", function () {
+    // calculator.setValues(2, 3);  //values for the tests
+
+    it("checks initial values 2 and 3", function () {
+        assert.strictEqual(calculator.a, 2);
+        assert.strictEqual(calculator.b, 3);
+    });
+
+    it("when 2 and 3 are entered, the sum is 5", function () {
+        assert.strictEqual(calculator.sum(), 5);
+    });
+
+    it("when 2 and 3 are entered, the product is 6", function () {
+        assert.strictEqual(calculator.mul(), 6);
+    });
+
+
+});
+
+
+
+describe("accumulator", function () {
+
+    it("checks initialValue and increment", function () {
+        const accumulator = new Accumulator(5, 10);
+        assert.strictEqual(accumulator.currentValue, 5);
+        assert.strictEqual(accumulator.increment, 10);
+    });
+
+    it("checks accumulate", function () {
+        const accumulator = new Accumulator(5, 10);
+        accumulator.accumulate();
+        accumulator.accumulate();
+        assert.strictEqual(accumulator.currentValue, 25);
+        assert.strictEqual(accumulator.increment, 10);
+    });    
+
+    it("checks report", function () {
+        const accumulator = new Accumulator(5, 10);
+        assert.strictEqual(accumulator.report(), 5);
+    });    
+
+});
